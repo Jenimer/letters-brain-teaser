157f173905edaabfc2e453b7b2cfb3abd2554ad2
Jen Broadhead <jeniffer.broadhead@gmail.com>
2018-06-15 10:37:34 -0600

    initial commit

---
+++ letters.rb
@@ -0,0 +1,16 @@
+
+#takes in a sentence
+
+puts "Enter a sentence: "
+input = gets.chomp
+arr = []
+
+#separates the sentence by word
+array = []
+array << input.split
+
+#counts repeated letters in a word
+
+#pulls out the word with the most repeated letters
+
+#gives response
